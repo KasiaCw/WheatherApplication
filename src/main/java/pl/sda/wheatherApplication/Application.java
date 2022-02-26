6a6a77eb7ba73b322e1759c930122c19c82ea11f
Michal Paukszto <michal.paukszto@gmail.com>
2022-02-26 11:19:48 +0100

    message

--- src/main/java/pl/sda/wheatherApplication/Application.java
+++ src/main/java/pl/sda/wheatherApplication/Application.java
@@ -3,5 +3,6 @@ package pl.sda.wheatherApplication;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Michal tu byl");
     }
 }
