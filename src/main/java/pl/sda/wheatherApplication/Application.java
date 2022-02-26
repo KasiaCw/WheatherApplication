47141c7feadb0decfe1283e14c6e403850df5f2f
Kasia <kasia@cwiakalska.com>
2022-02-26 11:59:37 +0100

    UsrInterface

--- src/main/java/pl/sda/wheatherApplication/Application.java
+++ src/main/java/pl/sda/wheatherApplication/Application.java
@@ -2,7 +2,7 @@ package pl.sda.wheatherApplication;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Michal tu byl");
+        UserInterface userInterface = new UserInterface();
+        userInterface.run();
     }
 }
