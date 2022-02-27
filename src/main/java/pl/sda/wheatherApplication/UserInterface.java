f9f0658140562d5385d75348fc5c2f68d0ca7bab
Michal Paukszto <michal.paukszto@gmail.com>
2022-02-27 14:21:11 +0100

    merge test

--- src/main/java/pl/sda/wheatherApplication/UserInterface.java
+++ src/main/java/pl/sda/wheatherApplication/UserInterface.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 
 public class UserInterface {
 
-    public void run(){
+    public void run() {
         System.out.println("STARTUJEMY\n");
 
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             System.out.println("Startujemy z info p pogodzie, co chcesz zrobić?");
+            System.out.println("1. hehe");
             System.out.println("0. Kończymy na dziś");
 
             int option = scanner.nextInt();
@@ -21,6 +22,4 @@ public class UserInterface {
             }
         }
     }
-
-
 }
